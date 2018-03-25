8b56b84d9a29d6f56fc6c3864d362d0e520ab2c0
Arshd <arshad@sort6.com>
2018-03-25 14:40:30 +0200

    Test

---
+++ Assets/Scripts/Force.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Force : MonoBehaviour {
+
+	public float x = 50f;
+	public float y = 10f;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.X))
+		{
+			rb.AddForce(new Vector2(x, y), ForceMode2D.Impulse);
+		}
+	}
+}
