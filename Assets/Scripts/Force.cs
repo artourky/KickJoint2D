242b56b4aa6b6c9ad90556d92a08e71600dcb25b
Arshd <arshad@sort6.com>
2018-03-26 00:39:22 +0200

    mod

--- Assets/Scripts/Force.cs
+++ Assets/Scripts/Force.cs
@@ -3,11 +3,21 @@
 [RequireComponent(typeof(Rigidbody2D))]
 public class Force : MonoBehaviour {
 
+	public class amon {
+		public int a;
+
+		public void data() {
+			print("" + a);
+		}
+	}
+
 	public Vector2 forceApplied = new Vector2(80f, 15f);
 	private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
+		amon a = new amon();
+		a.data();
 		rb = GetComponent<Rigidbody2D>();
 	}
 	
